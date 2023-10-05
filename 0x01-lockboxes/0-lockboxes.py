4b0cbf4b46e58cd9eab518a1cd99f977a7a7e3d7
peter njihia <peternjihia2001@gmail.com>
2023-10-05 16:06:46 +0000

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True
+    keys = [0]
+
+    while keys:
+        box = keys.pop()
+        for key in boxes[box]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                keys.append(key)
+
+    return all(unlocked)
+
+boxes1 = [[1], [2], [3], [4], []]
+print(canUnlockAll(boxes1))
+
+boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+print(canUnlockAll(boxes2))
+
+boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+print(canUnlockAll(boxes3))
