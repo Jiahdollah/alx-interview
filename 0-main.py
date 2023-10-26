06121a6bb61f513198228685597df9435d79e265
peter njihia <peternjihia2001@gmail.com>
2023-10-26 20:13:49 +0000

    utf8 validation

--- 0-main.py
+++ 0-main.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-0-main
+Main file for testing
 """
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
-def print_triangle(triangle):
-    """
-    Print the triangle
-    """
-    for row in triangle:
-        print("[{}]".format(",".join([str(x) for x in row])))
+validUTF8 = __import__('0-validate_utf8').validUTF8
 
+data = [65]
+print(validUTF8(data))
 
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
+
+data = [229, 65, 127, 256]
+print(validUTF8(data))
--- 0-validate_utf8.py
+++ 0-validate_utf8.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python3
-"""UTF-8 Validation Module
-"""
+"""determines valid UTF-8 encoding"""
 
 
 def validUTF8(data):
-    """determines if a given data set represents a valid UTF-8 encoding.
-
-    Args:
-        data (list): list of integers
-
-    returns:
-        bool: True if data is a valid UTF-8 encoding, else return False
     """
-    n_bytes = 0
+    bit1 checks if significant byte is 1
+    bit2 checks if second significant byte is 0
+    nbytes keeps track of how many 1s before 0 occurs
+    data represented by a list of integers to check
+    """
+
+    bit1 = 1 << 7
+    bit2 = 1 << 6
+    nbytes = 0
+
+    if not data or len(data) == 0:
+        return True
 
     for num in data:
-        byte = num & 0xff
-        if n_bytes == 0:
-            mask1 = 1 << 7
-            mask2 = 1 << 6
-            while mask1 & byte:
-                n_bytes += 1
-                mask1 = mask1 >> 1
-                mask2 = mask2 >> 1
-            if n_bytes == 0:
+        bit = 1 << 7
+        if nbytes == 0:
+            while (bit & num):
+                nbytes += 1
+                bit = bit >> 1
+
+            if nbytes == 0:
                 continue
-            if n_bytes == 1 or n_bytes > 4:
+            if nbytes == 1 or nbytes > 4:
                 return False
         else:
-            mask1 = 1 << 7
-            mask2 = 1 << 6
-            if not (byte & mask1 and not (byte & mask2)):
+
+            if not (num & bit1 and not (num & bit2)):
                 return False
-        n_bytes -= 1
-    return n_bytes == 0
+        nbytes -= 1
+
+    if nbytes:
+        return False
+    else:
+        return True
