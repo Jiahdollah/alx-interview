04c80fff97711028cb83d4e69a75db5c1a4beb14
peter njihia <111152898+Jiahdollah@users.noreply.github.com>
2023-11-10 00:02:12 +0300

    star war character

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,7 +8,7 @@ const filmURL = 'https://swapi-api.hbtn.io/api/films/';
 request(filmURL + filmNum, async function (err, res, body) {
   if (err) return console.error(err);
 
-  // find URLs of each character in the film as a list obj
+  // Find URLs of each character in the film as a list obj
   const charURLList = JSON.parse(body).characters;
 
   // Use URL list to character pages to make new requests
@@ -18,7 +18,7 @@ request(filmURL + filmNum, async function (err, res, body) {
       request(charURL, function (err, res, body) {
         if (err) return console.error(err);
 
-        // finds each character name and prints in URL order
+        // finds character name and prints in URL order
         console.log(JSON.parse(body).name);
         resolve();
       });
